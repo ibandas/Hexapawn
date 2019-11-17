3c1b0e5be94c44eb925f4a84956dcda975fc69d1
Madhav <madhavkhanna2021@u.northwestern.edu>
2019-11-17 14:49:57 -0600

    Defined Structure

--- src/Model.cxx
+++ src/Model.cxx
@@ -1,1 +1,50 @@
 #include "Model.hxx"
+
+
+void Model::choose_moves(int move_number) {
+
+}
+
+Model::Model() {
+
+}
+
+Model::Player Model::get_winner() const {
+    return {};
+}
+
+Model::Player::Player() {
+
+}
+
+int Model::Player::get_pawns() const {
+    return 0;
+}
+
+void Model::Player::MakeMove(int move) {
+
+}
+
+void Model::print_legal_moves() const {
+
+}
+
+Model::Player Model::get_currentPlayer() const {
+    return {};
+}
+
+bool Model::is_game_over() const {
+    return false;
+}
+
+int Model::get_legal_moves() {
+    return 0;
+}
+
+bool Model::Check_MoveForward(Model::Point current_position) {
+    return false;
+}
+
+bool Model::Check_MoveDiagonal(Model::Point current_position) {
+    return false;
+}
--- src/Model.hxx
+++ src/Model.hxx
@@ -2,5 +2,47 @@
 
 class Model
 {
+public:
 
+    Model();
+
+    struct Point;
+    class Player;
+
+    void choose_moves(int move_number);
+
+    Player get_winner() const;
+    void print_legal_moves() const;
+    bool is_game_over() const;
+    Player get_currentPlayer() const;
+
+private:
+    Point[] get_legal_moves();
+
+    bool Check_MoveForward(Point current_position);
+    bool Check_MoveDiagonal(Point current_position);
 };
+
+
+struct Model::Point{
+    int x = 0;
+    int y = 0;
+};
+
+class Model::Player
+{
+public:
+    enum class Name {A, B};
+    Player();
+    Point[] get_pawns() const;
+
+    void MakeMove(int move);
+};
+
+
+
+
+
+
+
+
