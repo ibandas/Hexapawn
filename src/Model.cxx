6840410ecd9bb0dffd3249637c346c392ec435d7
Irinel Bandas <ibandas@luc.edu>
2019-11-17 21:01:39 -0600

    create board

--- src/Model.cxx
+++ src/Model.cxx
@@ -1,5 +1,5 @@
 #include "Model.hxx"
-
+#include <vector>
 
 void Model::choose_moves(int move_number) {
 
@@ -17,7 +17,7 @@ Model::Player::Player() {
 
 }
 
-int Model::Player::get_pawns() const {
+std::vector<Model::Point> Model::Player::get_pawns() const {
     return 0;
 }
 
@@ -37,7 +37,8 @@ bool Model::is_game_over() const {
     return false;
 }
 
-int Model::get_legal_moves() {
+std::vector<Model::Point> Model::get_legal_moves() {
+    struct Point point;
     return 0;
 }
 
@@ -47,4 +48,28 @@ bool Model::Check_MoveForward(Model::Point current_position) {
 
 bool Model::Check_MoveDiagonal(Model::Point current_position) {
     return false;
+}
+
+void Model::create_board(int inputSize) {
+
+    // Create a vector of vector with size
+    // equal to row.
+    std::vector<std::vector<Player::User>> vec(inputSize);
+    Player::User A = Player::User::A;
+    Player::User B = Player::User::B;
+    Player::User N = Player::User::N;
+
+    for (int i = 0; i < inputSize; i++) {
+        for (int j = 0; j < inputSize; j++) {
+            if (i == 0) {
+                vec[i][j] = A;
+            }
+            else if (i == inputSize - 1) {
+                vec[i][j] = B;
+            }
+            else {
+                vec[i][j] = N;
+            }
+        }
+    }
 }
--- src/Model.hxx
+++ src/Model.hxx
@@ -1,23 +1,27 @@
 #pragma once
+#include <vector>
 
 class Model
 {
 public:
 
-    Model();
+    Model()
+    {
+
+    }
 
     struct Point;
     class Player;
 
+    void create_board(int inputSize);
     void choose_moves(int move_number);
-
     Player get_winner() const;
     void print_legal_moves() const;
     bool is_game_over() const;
     Player get_currentPlayer() const;
 
 private:
-    Point[] get_legal_moves();
+    std::vector<Point> get_legal_moves();
 
     bool Check_MoveForward(Point current_position);
     bool Check_MoveDiagonal(Point current_position);
@@ -25,19 +29,21 @@ private:
 
 
 struct Model::Point{
-    int x = 0;
-    int y = 0;
+    int x, y;
 };
 
 class Model::Player
 {
 public:
-    enum {A, B};
     Player();
-    Point[] get_pawns() const;
-
+    // Player is either A or B. N stands for Null and used to represent an empty space
+    enum class User {
+        A, B, N
+    };
+    std::vector<Point> get_pawns() const;
     void MakeMove(int move);
 };
+
 
 
 
