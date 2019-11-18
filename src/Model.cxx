3949bcea2110220fa741f05e172f9b6c3e9433be
Madhav <madhavkhanna2021@u.northwestern.edu>
2019-11-18 13:57:10 -0600

    Model [Updated]

--- src/Model.cxx
+++ src/Model.cxx
@@ -1,29 +1,49 @@
 #include "Model.hxx"
 #include <vector>
+#include <stdexcept>
+
+
+Model::Model()
+        : Model(3,3) {
+}
+
+Model::Model(int rows, int columns)
+       :board_(rows * columns),
+       A_{Model::Player::A},
+       B_{Model::Player::B}{
+    if(rows < 3 || columns < 3){
+        throw std::invalid_argument("Model::Model requires a board of size more than 3 x 3");
+    }
+}
 
 void Model::choose_moves(int move_number) {
-
 }
 
-Model::Model() {
 
-}
 
 Model::Player Model::get_winner() const {
     return {};
 }
 
-Model::Player::Player() {
-
-}
-
 std::vector<Model::Point> Model::Player::get_pawns() const {
-    return 0;
+    return pawns_;
+}
+void Model::Player::MakeMove(int move) {
+
 }
 
-void Model::Player::MakeMove(int move) {
-    Model::Player::A;
+void Model::Player::set_pawns(Model::Point position) {
+    Model::Player::pawns_.push_back(position);
 }
+
+void Model::Player::addMoves(Model::Point moveAvailable) {
+    Model::Player::moves_available_.push_back(moveAvailable);
+}
+
+std::vector<Model::Point> Model::Player::getMoves() const {
+    return moves_available_;
+}
+
 
 void Model::print_legal_moves() const {
 
@@ -36,40 +56,84 @@ Model::Player Model::get_currentPlayer() const {
 bool Model::is_game_over() const {
     return false;
 }
-
-std::vector<Model::Point> Model::get_legal_moves() {
-    struct Point point;
-    return 0;
+std::vector<Model::Point> Model::get_legal_moves(const Model::Player &player) const {
+    return player.getMoves();
 }
 
-bool Model::Check_MoveForward(Model::Point current_position) {
-    return false;
+bool Model::check_move_forward(Model::Point current_position) const {
+    Model::OccupiedPlace next_place_ = board_[current_position.x][current_position.y + 1];
+    return !(next_place_ == Model::OccupiedPlace::B || next_place_ == Model::OccupiedPlace::W);
 }
 
-bool Model::Check_MoveDiagonal(Model::Point current_position) {
-    return false;
+bool Model::check_move_left_diagonal(Model::Point position) const {
+    Model::OccupiedPlace current_position_ = board_[position.x][position.y];
+    Model::OccupiedPlace left_diagonal_ = board_[position.x - 1][position.y+1];
+    switch (current_position_) {
+        case Model::OccupiedPlace::B :
+            return left_diagonal_ == Model::OccupiedPlace::W;
+        case Model::OccupiedPlace::W :
+            return left_diagonal_ == Model::OccupiedPlace::B;
+        default:
+            throw std::invalid_argument("No Pawn at the Current Index");
+    }
 }
 
-void Model::create_board(int inputSize) {
+bool Model::check_move_right_diagonal(Model::Point position) const {
+    Model::OccupiedPlace current_position_ = board_[position.x][position.y];
+    Model::OccupiedPlace right_diagonal_ = board_[position.x + 1][position.y + 1];
+    switch (current_position_) {
+        case Model::OccupiedPlace::B :
+            return right_diagonal_ == Model::OccupiedPlace::W;
+        case Model::OccupiedPlace::W :
+            return right_diagonal_ == Model::OccupiedPlace::B;
+        default:
+            throw std::invalid_argument("No Pawn at the Current Index");
+    }
+}
 
-    // Create a vector of vector with size
-    // equal to row.
-    std::vector<std::vector<Player::User>> vec(inputSize);
-    Player::User A = Player::User::A;
-    Player::User B = Player::User::B;
-    Player::User N = Player::User::N;
+void Model::populate_board(int rows, int columns) {
+    Model::Player playerB;
+    Model::Player playerA;
+    Model::OccupiedPlace B = Model::OccupiedPlace::B;
+    Model::OccupiedPlace W = Model::OccupiedPlace::W;
+    Model::OccupiedPlace N = Model::OccupiedPlace::N;
 
-    for (int i = 0; i < inputSize; i++) {
-        for (int j = 0; j < inputSize; j++) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
             if (i == 0) {
-                vec[i][j] = A;
+                board_[i][j] = B;
+                Model::Point p = Point(i,j);
+                playerB.set_pawns(p);
             }
-            else if (i == inputSize - 1) {
-                vec[i][j] = B;
+            else if (i == rows - 1) {
+                board_[i][j] = W;
             }
             else {
-                vec[i][j] = N;
+                board_[i][j] = N;
             }
         }
     }
 }
+
+void Model::set_legal_moves(Model::Player &player) {
+
+    std::vector<Model::Point> allPawns_ = player.get_pawns();
+    for(auto allPawn : allPawns_) {
+        if(check_move_forward(allPawn)) {
+            Model::Point p(allPawn.x, allPawn.y+1);
+            player.addMoves(p);
+        }
+        if(check_move_right_diagonal(allPawn)) {
+            Model::Point p(allPawn.x-1, allPawn.y+1);
+            legalMoves_.push_back(p);
+        }
+        if(check_move_left_diagonal(allPawn)) {
+            Model::Point p(allPawn.x+1, allPawn.y+1);
+            legalMoves_.push_back(p);
+        }
+    }
+    return legalMoves_;
+}
+
+
+
--- src/Model.hxx
+++ src/Model.hxx
@@ -4,45 +4,62 @@
 class Model
 {
 public:
-
-    Model()
-    {
-
-    }
+    Model();
+    Model(int rows, int columns);
 
     struct Point;
     class Player;
 
-    void create_board(int inputSize);
+    // B -> Occupied by the Black Pawn.
+    // W -> Occupied by the White Pawn.
+    // N -> No Pawn at the current place.
+    enum class OccupiedPlace{
+        B, W, N
+    };
+
+    void populate_board(int rows, int columns);
     void choose_moves(int move_number);
     Player get_winner() const;
     void print_legal_moves() const;
     bool is_game_over() const;
     Player get_currentPlayer() const;
+    std::vector<Point> get_legal_moves(const Player& player) const;
+    void set_legal_moves(Player& player);
 
 private:
-    std::vector<Point> get_legal_moves();
+    std::vector< std::vector<OccupiedPlace> > board_;
+    Player A_;
+    Player B_;
+    bool check_move_forward(Point current_position) const;
+    bool check_move_left_diagonal(Point current_position) const;
+    bool check_move_right_diagonal(Point current_position) const;
 
-    bool Check_MoveForward(Point current_position);
-    bool Check_MoveDiagonal(Point current_position);
 };
-
 
 struct Model::Point{
     int x, y;
+    Point(int x, int y)
+    :x{x}, y{y}{};
 };
 
 class Model::Player
 {
 public:
     Player();
-    // Player is either A or B. N stands for Null and used to represent an empty space
-    enum class User {
-        A, B, N
-    };
+    // Player is either A or B.
+    enum Type { B, W };
     std::vector<Point> get_pawns() const;
+    void set_pawns(Model::Point position);
+    void addMoves(Point moveAvailable);
+    std::vector<Point> getMoves() const;
     void MakeMove(int move);
+
+private:
+    std::vector<Point> pawns_;
+    std::vector<Point> moves_available_;
 };
+
+
 
 
 
